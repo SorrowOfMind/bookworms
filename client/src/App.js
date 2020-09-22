ed32fa38cc17b9a9aef776c9af50aa9d4b3d20fb
Basia <b.kumor@yahoo.com>
2020-09-22 16:45:01 +0200

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Router>
+        <Routes />
+      </Router>
     </div>
   );
 }

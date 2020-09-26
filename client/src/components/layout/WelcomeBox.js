89c35d98151916ac8f3b34636d119f202613fd67
Basia <b.kumor@yahoo.com>
2020-09-26 12:22:48 +0200

    fix style

--- client/src/components/layout/WelcomeBox.js
+++ client/src/components/layout/WelcomeBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WelcomeBox = () => {
     return (
         <div className="welcome-box">
-            <h2 className="welcome-box__title">Lorem ipsum dolor sit amet elit</h2>
+            <h2 className="welcome-box__title">Lorem ipsum dolor sit amet</h2>
             <p className="welcome-box__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid iure molestiae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid iure molestiae.</p>
         </div>
     )
